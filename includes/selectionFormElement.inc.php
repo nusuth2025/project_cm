e553cb16f466ec66d60013f3a73a050f2f589f44
nusuth2025 <susev@linux.fritz.box>
2026-04-30 13:25:36 +0200

    kleine Textänderung in selectionFormElement, phpinfo.php entfernt

--- includes/selectionFormElement.inc.php
+++ includes/selectionFormElement.inc.php
@@ -6,7 +6,7 @@ class SelectionFormElement{
         $s = $_SERVER["PHP_SELF"];
         $this->selectionFormElement = 
         '<form action="'. $s.'" method="post">
-                <label for="wider_select">please copy-paste the data you\'d like to monitor from the website you selected enclosed in some surroundings </label>
+                <label for="wider_select">please copy-paste the data you\'d like to monitor from the website you selected enclosed with some surrounding lines </label>
                 <textarea type="text" name="wider_select" id="wider_select" col="30" row="30" placeholder="'.$formBox .'"  ></textarea>
                 <input type="submit" value="Raus damit">
                 <input type="reset" value="Ach Mist">
--- phpinfo.php
+++
@@ -1,4 +0,0 @@
-<?php
-xdebug_info(); //alles richtig auch wenn die Funktion nicht bekannt ist
-phpinfo();
-?>
